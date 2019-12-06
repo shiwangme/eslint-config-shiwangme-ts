e5ac872057a5aee86745cc7555d68150b7a31a9f
willin <willin@willin.org>
2019-12-06 19:15:05 +0800

    v1.0.3

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ module.exports = {
       { allowAfterThis: true, allowAfterSuper: false }
     ],
     'object-curly-spacing': 2,
+    'spaced-comment': [2, 'always', { markers: ['/'] }],
     '@typescript-eslint/explicit-function-return-type': 2,
     '@typescript-eslint/no-explicit-any': 2,
     '@typescript-eslint/no-unused-vars': 2,
